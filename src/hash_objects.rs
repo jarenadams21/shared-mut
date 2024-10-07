f40fd251da7af75c7f6f2657ab1af61e6443f40a
jaren adams <jarenadams@jarens-Laptop.local>
2024-10-07 11:59:49 -0400

    notes

--- src/hash_objects.rs
+++ src/hash_objects.rs
@@ -11,7 +11,25 @@ struct ByteEncodedQBNode {
 }
 
 struct ByteEncodedQBLeaf {
-    
+
+}
+
+/**
+ Holds data blocks containing information from classical web
+*/
+struct ClassicalWebDataSourceBlock {
+
+}
+
+/** I think the idea from this point is classical web data sources that got scraped
+    will process through the q system and become a QStateSourceBlock for search optimizations/processing.
+ */
+
+/**
+ Holds data blocks containing information from q system state
+*/
+struct QStateSourceBlock {
+
 }
 
 struct QSpace {
