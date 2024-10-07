1e762cb87d2b34dde7505e123a0b77c95209e824
jaren adams <jarenadams@jarens-Laptop.local>
2024-10-07 11:53:51 -0400

    byte scaffold

---
+++ src/hash_objects.rs
@@ -0,0 +1,19 @@
+/**
+
+Describes a custom byte encoded b-tree intended for q space
+*/
+struct ByteEncodedQBTree {
+
+}
+
+struct ByteEncodedQBNode {
+
+}
+
+struct ByteEncodedQBLeaf {
+    
+}
+
+struct QSpace {
+    internal_time_index: Unknown,
+}
