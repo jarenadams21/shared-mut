81ca118d0aed1adca71ec4915437728cf0462622
jaren adams <jarenadams@jarens-Laptop.local>
2024-10-07 14:32:08 -0400

    comment dont forget

--- src/sync_prims.rs
+++ src/sync_prims.rs
@@ -2,3 +2,8 @@ struct SyncPrimitive {
     byte_storm: /** the current state of pytes between classical, partial, and q, for testing snapshots */
     thread_safe: dont_know_yet,
 }
+
+// Looking for truths in random probably needed
+function delete_jira() {
+    
+}
